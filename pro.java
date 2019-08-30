37520d4d01c37e8379a14454de6dfc7d06784ec6
anvithapo99 <53563539+anvithapo99@users.noreply.github.com>
2019-08-30 16:08:43 +0530

    Update pro.java

--- pro.java
+++ pro.java
@@ -1,4 +1,4 @@
-Hacker and traffic lights
+/*Hacker and traffic lights
 Zolo is stuck in a traffic due to dysfunctional traffic light. Zolo is a professional hacker
 and he can get into the system and change the state of the light. His planet has
 different types of traffic lights such that there are N bulbs on the traffic board and
@@ -15,10 +15,8 @@ The first line contains N K
 The second line describes the current status of each bulb as an array whose each
 element can either be G or Y or R.
 Output Format
-Print the minimum amount of time required to clear the traffic jam".
-sample input       sample output
-4 2                5
-R X G Y
+Print the minimum amount of time required to clear the traffic jam".*/
+
 import java.util.Scanner;
  
 class TestClass {
@@ -54,3 +52,8 @@ class TestClass {
         }
     }
 }
+/*sample input       sample output
+4 2                5
+R X G Y  */
+
+
